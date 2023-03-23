3eb1ab4531ea39e7ef4a75d6369138d1777f4db8
Nick <nhranitz@gmail.com>
2023-03-23 15:13:16 -0400

    home page, prettier fix because 2 spaces is just wrong

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,5 @@
+import House from '../assets/HomePage.png'
+
 export default function Home(params) {
-  return <div>HomeChain Intro</div>;
+  return <div>HomeChain Intro <img className='m-auto' width='80%' src={House} alt="house" /></div>;
 }

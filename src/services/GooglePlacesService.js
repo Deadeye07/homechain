e7e36c1d39cfa93f0bca69b51121eb80af700761
victoria bottali <victoriab@sca-corp.com>
2023-03-24 09:36:21 -0400

    aws url

--- src/services/GooglePlacesService.js
+++ src/services/GooglePlacesService.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default class GooglePlacesService {
-    static baseUrl = 'http://localhost:8000/';
+    //static baseUrl = 'http://localhost:8000/';
+    static baseUrl = 'https://a4r7lrhpce.execute-api.us-east-1.amazonaws.com/'
 
     static async getPlaceDetails(placeId) {
        return await axios
